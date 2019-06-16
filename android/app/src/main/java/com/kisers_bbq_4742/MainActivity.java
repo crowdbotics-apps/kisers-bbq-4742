8955a12244e30908c27d5ee7870bf2b8eb1965e0
Crowdbotics <team@crowdbotics.com>
2019-06-16 13:03:04 +0000

    initial commit

---
+++ android/app/src/main/java/com/kisers_bbq_4742/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kisers_bbq_4742;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "kisers_bbq_4742";
+    }
+}
